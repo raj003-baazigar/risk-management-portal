6c82cf33603e51bc9528c773fc9f05ae597e0d09
raj003-baazigar <agrawalraj915@gmail.com>
2025-11-30 11:52:31 +0530

    Added application variable for AWS deployment

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
+application = app  # For some deployment platforms
 app.secret_key = "change_this_to_a_secure_random_key"
 
 # Database
